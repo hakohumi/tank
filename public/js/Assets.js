1a6bc36541793974af2d7f44da4a859be5c1228e
filu <mihumihu32@gmail.com>
2023-04-22 20:08:43 +0900

    ✨ Add client-side source code

---
+++ public/js/Assets.js
@@ -0,0 +1,11 @@
+// アセット群クラス
+class Assets {
+  // コンストラクタ
+  constructor() {
+    // 背景画像
+    this.imageField = new Image()
+    this.imageField.src = '../images/grass01.png'
+
+    this.rectFieldInFieldImage = { sx: 0, sy: 0, sw: 512, sh: 512 }
+  }
+}
